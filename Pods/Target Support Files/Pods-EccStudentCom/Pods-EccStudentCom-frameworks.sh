b2aa4aea79171bc59abcc4bd1ee43f91c9cdb066
YutaKohashi <kaerukerokero1995@gmail.com>
2017-02-22 10:15:43 +0900

    no message

--- Pods/Target Support Files/Pods-EccStudentCom/Pods-EccStudentCom-frameworks.sh
+++ Pods/Target Support Files/Pods-EccStudentCom/Pods-EccStudentCom-frameworks.sh
@@ -87,11 +87,9 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/KRProgressHUD/KRProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/XLPagerTabStrip/XLPagerTabStrip.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/KRProgressHUD/KRProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/XLPagerTabStrip/XLPagerTabStrip.framework"
 fi
--- Pods/Target Support Files/XLPagerTabStrip/XLPagerTabStrip-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double XLPagerTabStripVersionNumber;
-FOUNDATION_EXPORT const unsigned char XLPagerTabStripVersionString[];
-
